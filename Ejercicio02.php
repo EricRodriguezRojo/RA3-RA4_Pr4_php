0d9aa9115b10ddc3d0ef1cc7aead91a58041995b
Eric Rodriguez Rojo <erpys02@gmail.com>
2025-02-16 13:05:26 +0100

    Errores arreglados

--- Ejercicio02.php
+++ Ejercicio02.php
@@ -5,16 +5,16 @@ $position = $_POST['position']; //guardamos la posicion y el valor en 2 variable
 $value = $_POST['value'];
 
 if (!isset($_SESSION['lista'])) {
-    $_SESSION['lista'] = [array(10, 20, 30)];
+    $_SESSION['lista'] = array(10, 20, 30);
 }
 
-if (isset($_POST['modify'])) { //modificar sesion lista
-    if($position == 1){
-        $_SESSION['lista'][0] = $value;
-    }else if($position == 2){
-        $_SESSION['lista'][1] = $value;
-    }else{
-        $_SESSION['lista'][2] = $value;
+if (isset($_POST['modify'])) { // modificamos la posicion
+    $position = (int)$_POST['position'] - 1; 
+
+    if ($position >= 0 && $position < count($_SESSION['lista'])) {
+        $_SESSION['lista'][$position] = $value;
+    } else {
+        echo "Error: Posición fuera de rango.";
     }
 }
 
@@ -66,15 +66,16 @@ if (isset($_POST['reset'])) { //reseteamos la sesion lista
             }
         }
         
-        if (isset($_POST['average'])) { //Hacer la media de los 3 valores actuales en el array
+        if (isset($_POST['average'])) {
             $count = count($_SESSION['lista']);
-            $total = 0;
-
-            foreach ($_SESSION['lista'] as $value) {
-                $total += $value;
+            
+            if ($count > 0) {
+                $total = array_sum($_SESSION['lista']);
+                $average = $total / $count;
+                echo "Average: $average";
+            } else {
+                echo "Error: No hay valores en la lista.";
             }
-            $totalmedia = $total/$count;
-            echo "Average: $totalmedia";
         }
         ?>
 
